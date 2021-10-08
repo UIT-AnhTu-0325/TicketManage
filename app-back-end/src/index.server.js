35c039fff6b4c66be52c69e740345af20bcfdead
Đặng Anh Tú <danganhtu2001@gmail.com>
2021-10-09 01:49:39 +0700

    update project

---
+++ app-back-end/src/index.server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.js");
+const adminRoutes = require("./routes/admin/auth");
+const cors = require("cors");
+
+//env var
+env.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@ticketmanage.e4mp6.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
