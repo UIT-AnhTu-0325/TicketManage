39c17954626f80c893b99339e6ea6f95fc09142e
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-10-20 14:04:53 +0700

    create front end 'login +home '

--- tempHong/TicketManage-main/admin-app/src/temp/api/ticket.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const url = 'http://localhost:2000/api/ticket'
-
-export const getAll = () => axios.get(url);
-export const getById = (id) => axios.get(`${url}/${id}`);
-export const createNew = (newTicket) => axios.post(url, newTicket);
