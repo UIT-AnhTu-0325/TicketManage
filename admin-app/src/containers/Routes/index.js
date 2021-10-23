c3263c978d23b7cd7ba35b1ac90b0edc12868d38
Đặng Anh Tú <danganhtu2001@gmail.com>
2021-10-24 00:11:55 +0700

    update

---
+++ admin-app/src/containers/Routes/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Layout } from "../../components/Layout";
+
+/**
+ * @author
+ * @function Routes
+ **/
+
+export const Routes = (props) => {
+  return <Layout sidebar>Routes</Layout>;
+};
--- admin-app/src/reducers/index.js
+++ admin-app/src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth.reducers";
 import userReducer from "./user.reducer";
+import routeReducer from "./route.reducers";
+import enterpriseReducer from "./enterprise.reducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  user: userReducer
+  user: userReducer,
+  route: routeReducer,
+  enterprise: enterpriseReducer,
 });
 
 export default rootReducer;
