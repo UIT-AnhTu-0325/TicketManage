0183f8088ca1c2661485ecc299a1315a2dccd9bf
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-11-10 16:44:56 +0700

    update minor fixbug

--- admin-app/src/components/sidebar/SidebarItem.js
+++ admin-app/src/components/sidebar/SidebarItem.js
@@ -1,26 +1,20 @@
-import React from 'react'
+import React from "react";
 
 /**
-* @author
-* @function SidebarItem
-**/
+ * @author
+ * @function SidebarItem
+ **/
 
 export const SidebarItem = (props) => {
-
-    const active= props.active ? 'active' : ''
-    return (
-        <div>
-            <div className="sidebar__item" onClick={props.changeTab}>
-                <div className={`sidebar__item-inner ${active}`}>
-                    <i className={props.icon}></i>
-                    <span>
-                        {props.title}
-                    </span>
-                </div>
-                
-            </div>
-
+  const active = props.active ? "active" : "";
+  return (
+    <div>
+      <div className="sidebar__item">
+        <div className={`sidebar__item-inner ${active}`}>
+          <i className={props.icon}></i>
+          <span>{props.title}</span>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
