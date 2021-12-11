dbc8e7b28f3bc35003de43ab701d1c64c9b3c9d8
Đặng Anh Tú <danganhtu2001@gmail.com>
2021-12-12 04:09:46 +0700

    update

--- admin-app/src/components/dropdown/DropDown.js
+++ admin-app/src/components/dropdown/DropDown.js
@@ -1,74 +1,66 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
-import './dropdown.css'
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { signout } from "../../actions";
+import "./dropdown.css";
 
 /**
-* @author
-* @function DropDown
-**/
-const clickToggle = (contentRef,  toggleRef ) =>{
-    document.addEventListener('mousedown', (e) =>{
-        if(toggleRef.current &&toggleRef.current.contains(e.target)){
-            contentRef.current.classList.add("active")
-        }
-        else
-        if(contentRef.current && !contentRef.current.contains(e.target)){
-            contentRef.current.classList.remove("active")
-        }
-        
-    })
-}
-
+ * @author
+ * @function DropDown
+ **/
+const clickToggle = (contentRef, toggleRef) => {
+  document.addEventListener("mousedown", (e) => {
+    if (toggleRef.current && toggleRef.current.contains(e.target)) {
+      contentRef.current.classList.add("active");
+    } else if (contentRef.current && !contentRef.current.contains(e.target)) {
+      contentRef.current.classList.remove("active");
+    }
+  });
+};
 
 export const DropDown = (props) => {
+  const dispatch = useDispatch();
 
-    const renderNotifyItem = (item, ind) =>{
-        return (
-            <div className="notify-item" key={ind}>
-                <i className={item.icon}></i>
-                <span>{item.content}</span>
-            </div>
-        )
-    }
-   
-    const  dropToggle_el= useRef(null)
-    const dropContent_el =useRef(null)
-    clickToggle(dropContent_el, dropToggle_el)
-    
+  const logout = () => {
+    dispatch(signout());
+  };
+  const renderNotifyItem = (item, ind) => {
     return (
-        <div className="dropdown">
-            <button ref={dropToggle_el} className="dropdown__toggle">
-                {
-                    props.icon ? <i className={props.icon}></i> : ""
+      <div className="notify-item" key={ind} onClick={logout}>
+        <i className={item.icon}></i>
+        <span>{item.content}</span>
+      </div>
+    );
+  };
 
-                }
-                {
-                    props.badge ? <span className="dropdown__toggle-badge">{props.badge}</span> : ""
-                }
-                {
-                    props.customToggle ? props.customToggle() : ""
-                }
-            </button>
+  const dropToggle_el = useRef(null);
+  const dropContent_el = useRef(null);
+  clickToggle(dropContent_el, dropToggle_el);
 
-            <div ref={dropContent_el} className="dropdown__content">
-                {
-                    props.contentData && props.renderItems ? 
-                    props.contentData.map((item, ind)=>(
-                        renderNotifyItem(item, ind)
-                    )) : ""
-                }
-                {
-                    props.renderFooter ? (
-                        <div className="dropdown__footer">
-                            <Link to='/'>Xem tất cả </Link>
-                        </div>
-                    ) : ""
-                }
+  return (
+    <div className="dropdown">
+      <button ref={dropToggle_el} className="dropdown__toggle">
+        {props.icon ? <i className={props.icon}></i> : ""}
+        {props.badge ? (
+          <span className="dropdown__toggle-badge">{props.badge}</span>
+        ) : (
+          ""
+        )}
+        {props.customToggle ? props.customToggle() : ""}
+      </button>
 
-
-                
-            </div>
-        </div>
-    )
-
-}
+      <div ref={dropContent_el} className="dropdown__content">
+        {props.contentData && props.renderItems
+          ? props.contentData.map((item, ind) => renderNotifyItem(item, ind))
+          : ""}
+        {props.renderFooter ? (
+          <div className="dropdown__footer">
+            <Link to="/">Xem tất cả </Link>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+};
--- app-back-end/src/controller/admin/auth.js
+++ app-back-end/src/controller/admin/auth.js
@@ -16,7 +16,8 @@ exports.signup = async (req, res) => {
       });
   });
 
-  const { firstName, lastName, email, password, username, contactNumber } = req.body;
+  const { firstName, lastName, email, password, username, contactNumber } =
+    req.body;
   const hash_password = await bcrypt.hash(password, 10);
   const _user = new User({
     firstName,
@@ -25,7 +26,7 @@ exports.signup = async (req, res) => {
     hash_password,
     username,
     role: "admin",
-    contactNumber
+    contactNumber,
   });
 
   _user.save((error, data) => {
@@ -42,40 +43,44 @@ exports.signup = async (req, res) => {
   });
 };
 
-exports.signin = (req, res) => {
+exports.signin = async (req, res) => {
+  let myUser;
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (error) return res.status(400).json({ error });
-    if (user) {
-      if (user.authenticate(req.body.password) && user.role === "admin") {
-        const token = jwt.sign(
-          { id: user.id, role: user.role },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "1h",
-          }
-        );
-        res.cookie("token", token, { expiresIn: "1h" });
-        const { _id, firstName, lastName, email, role, fullName } = user;
-        res.status(200).json({
-          token,
-          user: {
-            _id,
-            firstName,
-            lastName,
-            email,
-            role,
-            fullName,
-          },
-        });
-      } else {
-        return res.status(400).json({
-          message: "Your username or password may be incorrect",
-        });
-      }
-    } else {
-      return res.status(400).json({ message: "Something went wrong" });
-    }
+    myUser = user;
   });
+  myUser = await User.findOne({ email: req.body.email });
+  if (myUser) {
+    const match = await bcrypt.compare(req.body.password, myUser.hash_password);
+    if (match && myUser.role === "admin") {
+      const token = jwt.sign(
+        { id: myUser.id, role: myUser.role },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.cookie("token", token, { expiresIn: "1h" });
+      const { _id, firstName, lastName, email, role, fullName } = myUser;
+      res.status(200).json({
+        token,
+        myUser: {
+          _id,
+          firstName,
+          lastName,
+          email,
+          role,
+          fullName,
+        },
+      });
+    } else {
+      return res.status(400).json({
+        message: "Your username or password may be incorrect",
+      });
+    }
+  } else {
+    return res.status(400).json({ message: "Something went wrong" });
+  }
 };
 
 exports.signout = (req, res) => {
