c9bb65635d3577b8a197bf5f85b9e088feecdb20
lamvanhongUIT19520094 <19520094@gm.uit.edu.vn>
2021-12-11 22:41:49 +0700

    complete issues (udpate ui ticket offlinebooking, print page ui)

--- admin-app/src/containers/OfflineTicket/index.js
+++ admin-app/src/containers/OfflineTicket/index.js
@@ -8,6 +8,7 @@ import { Layout } from "../../components/Layout";
 import { InputTitleLeft } from "../../components/UI/inputTitleLeft/InputTitleLeft";
 import { SelectBox } from "../../components/UI/select/SelectBox";
 
+import "./offlineticket.css";
 /**
  * @author
  * @function OfflineTicket
@@ -85,83 +86,148 @@ export const OfflineTicket = (props) => {
     }
     return listSeat;
   };
-
+  const math = (startTime, totalTime) => {
+    let s = startTime + totalTime;
+    if (s - Math.floor(s) > 0.59 || s - Math.floor(s) == 0) {
+      s += +1 - 0.6;
+    }
+    return s >= 24
+      ? (s - 24).toFixed(2).toString().replace(".", ":")
+      : s.toFixed(2).toString().replace(".", ":");
+  };
   return (
     <Layout sidebar>
-      <h1>.</h1>
-      <h1>Đặt vé offline</h1>
-      <h2>
-        Tuyến: {state_trip.idRoute.startLocation} -{" "}
-        {state_trip.idRoute.endLocation}
-      </h2>
-      <h2>Ngày đi: {state_trip.startDate}</h2>
-      <h2>Giờ đi: {state_trip.idRoute.startTime}</h2>
-      <h2>Thời gian đi: {state_trip.idRoute.totalTime}</h2>
-      {/* <h2>Số ghế: {localStorage.getItem("seatSelect")}</h2> */}
-      <SelectBox
-        value={offTicket.seatNumber}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, seatNumber: e.target.value });
-        }}
-        list={genSeatSelect()}
-        type="SeatSelect"
-        title="Số ghế"
-      />
-      <InputTitleLeft
-        title="Họ và tên"
-        value={offTicket.name}
-        placeholder={``}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, name: e.target.value });
-        }}
-      />
-      <InputTitleLeft
-        title="Số điện thoại"
-        value={offTicket.contactNumber}
-        placeholder={``}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, contactNumber: e.target.value });
-        }}
-      />
-      <InputTitleLeft
-        title="Ngày sinh"
-        value={offTicket.dob}
-        placeholder={``}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, dob: e.target.value });
-        }}
-      />
-      <InputTitleLeft
-        title="Địa chỉ"
-        value={offTicket.address}
-        placeholder={``}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, address: e.target.value });
-        }}
-      />
+      <div className="offlineticket">
+        <h3 className="head-title">Đặt vé offline</h3>
+        <div className="offlineticket__body">
+          <div className="left-pannel-info">
+            <div className="header">
+              <i class="cus-bus fas fa-bus"></i>
+              <span className="depart">{state_trip.idRoute.startLocation}</span>
+              <i class="cus-right fas fa-arrow-right"></i>
+              <span className="destination">
+                {state_trip.idRoute.endLocation}
+              </span>
+            </div>
+            <div className="main">
+              <div className="date">
+                <i class="fas fa-table"></i>
+                <span> Ngày khởi hành</span>
+                <p className="start-time">
+                  {" "}
+                  {new Date(state_trip.startDate).toLocaleDateString("vi-VN")}
+                </p>
+              </div>
+              <div className="bus-branch">
+                {/* <span> Xe</span> <spanc className="branch-name"></spanc> */}
+              </div>
+              <div className="time-estimate__wrapper">
+                <i class="far fa-clock"></i>
+                <span> Thời gian</span>
+              </div>
 
-      <SelectBox
-        value={offTicket.getOn}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, getOn: e.target.value });
-        }}
-        list={getLocationByCityName(state_trip.idRoute.startLocation)}
-        addShow={state_trip.idRoute.startLocation}
-        type="LocationSelect"
-        title="Nơi đón"
-      />
+              <div className="time-estimate">
+                <div className="time-site time-site-start">
+                  <span className="time">
+                    {state_trip.idRoute.startTime
+                      .toFixed(2)
+                      .toString()
+                      .replace(".", ":")}
+                  </span>
+                  <span className="site">
+                    {state_trip.idRoute.startLocation}
+                  </span>
+                </div>
+                <i class="fas fa-caret-right"></i>
+                <div className="time-site time-site-end">
+                  <span className="time">
+                    {math(
+                      state_trip.idRoute.startTime,
+                      state_trip.idRoute.totalTime
+                    )}
+                  </span>
+                  <span className="site">{state_trip.idRoute.endLocation}</span>
+                </div>
+              </div>
+              <div className="total-time__wrapper">
+                <i class="cus-dot fas fa-circle"></i>
+                <div className="time-taking-estimate">
+                  {state_trip.idRoute.totalTime} phút di chuyển
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="input-form">
+            {/* <h2>Số ghế: {localStorage.getItem("seatSelect")}</h2> */}
+            <SelectBox
+              value={offTicket.seatNumber}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, seatNumber: e.target.value });
+              }}
+              list={genSeatSelect()}
+              type="SeatSelect"
+              title="Số ghế"
+            />
+            <InputTitleLeft
+              title="Họ và tên"
+              value={offTicket.name}
+              placeholder={``}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, name: e.target.value });
+              }}
+            />
+            <InputTitleLeft
+              title="Số điện thoại"
+              value={offTicket.contactNumber}
+              placeholder={``}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, contactNumber: e.target.value });
+              }}
+            />
+            <InputTitleLeft
+              title="Ngày sinh"
+              value={offTicket.dob}
+              placeholder={``}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, dob: e.target.value });
+              }}
+            />
+            <InputTitleLeft
+              title="Địa chỉ"
+              value={offTicket.address}
+              placeholder={``}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, address: e.target.value });
+              }}
+            />
 
-      <SelectBox
-        value={offTicket.getOff}
-        onChange={(e) => {
-          setOffTicket({ ...offTicket, getOff: e.target.value });
-        }}
-        list={getLocationByCityName(state_trip.idRoute.endLocation)}
-        addShow={state_trip.idRoute.endLocation}
-        type="LocationSelect"
-        title="Nơi trả"
-      />
-      <Button onClick={createOfflineTicket}>Hoàn thành</Button>
+            <SelectBox
+              value={offTicket.getOn}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, getOn: e.target.value });
+              }}
+              list={getLocationByCityName(state_trip.idRoute.startLocation)}
+              addShow={state_trip.idRoute.startLocation}
+              type="LocationSelect"
+              title="Nơi đón"
+            />
+
+            <SelectBox
+              value={offTicket.getOff}
+              onChange={(e) => {
+                setOffTicket({ ...offTicket, getOff: e.target.value });
+              }}
+              list={getLocationByCityName(state_trip.idRoute.endLocation)}
+              addShow={state_trip.idRoute.endLocation}
+              type="LocationSelect"
+              title="Nơi trả"
+            />
+            <Button className="btn-create-ticket" onClick={createOfflineTicket}>
+              Hoàn thành
+            </Button>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
